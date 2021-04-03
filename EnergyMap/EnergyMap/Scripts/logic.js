af08edd5a5b29f0696a8bf6d290b39064b267c63
NikitaShkulov <tyuiop_2@mail.ru>
2021-04-03 20:56:14 +0700

    Создание основы проекта.

---
+++ EnergyMap/EnergyMap/Scripts/logic.js
@@ -0,0 +1,8 @@
+﻿//отображение карты
+var mymap = L.map("mapid").setView([51.505, -0.09], 5);
+
+//первичное создание карты
+geojson = new L.geoJson(bounds, {
+    style: initialStyle,
+    onEachFeature: onEachFeature,
+}).addTo(mymap);
